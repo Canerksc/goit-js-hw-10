14dd8468796ba1fce9e63858b3ce4369476f1780
Caner <canerkesici08@gmail.com>
2025-06-10 20:23:56 +0300

    main.js-delete

--- src/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#timer-btn").addEventListener("click", () => {
-  window.location = "1-timer.html";
-});
-document.querySelector("#snackbar-btn").addEventListener("click", () => {
-  window.location = "2-snackbar.html";
-});
