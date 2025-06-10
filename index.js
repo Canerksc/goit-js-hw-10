48592eb6ad762b1061e8dc1133baa3e91638d107
Canerksc <canerkesici08@gmail.com>
2025-06-10 17:25:35 +0000

    Deploying to gh-pages from @ Canerksc/goit-js-hw-10@f76c30d1728259f13525f9e2c6bec6bc619406e5 🚀

--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-DGiBSD1R.js";document.querySelector("#timer-btn").addEventListener("click",()=>{window.location="1-timer.html"});document.querySelector("#snackbar-btn").addEventListener("click",()=>{window.location="2-snackbar.html"});
-//# sourceMappingURL=index.js.map
